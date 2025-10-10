80712fa3703e0d9ca7097438c42917364afc22cb
ChaeHaejoong <svneh0329@naver.com>
2025-10-10 20:05:20 +0900

    feat(server): nest initial & design game module sturcture

---
+++ server/src/game/entity/player.ts
@@ -0,0 +1,10 @@
+export default class Player {
+  name: string;
+  coin: number;
+  
+  constructor(name: string) {
+    this.name = name;
+  }
+  
+  
+}
---
+++ server/src/game/game-engine.ts
@@ -0,0 +1,22 @@
+import Player from "./entity/player";
+
+export class GameEngine {
+  private deck = Array(15);
+  private playerList;
+  
+  constructor(playerList: Player[]) {
+    this.playerList = playerList;
+  }
+  
+  public init() {
+    
+  }
+  
+  private setDeck() {
+    
+  }
+  
+  private shuffleDeck() {
+    
+  }
+} 
