66406da3049ca6b4a549eb2f720532b393f38f64
ChaeHaejoong <svneh0329@naver.com>
2025-10-11 13:52:01 +0900

    wip(client/commands/room): creating feature

--- client/src/commands/room/roomCommandHandler.ts
+++ client/src/commands/room/roomCommandHandler.ts
@@ -1,6 +1,26 @@
+import { SokcetLocation } from "../../enums/socketLocation";
 import { SubCommandParams } from "../types";
+import { checkLocation } from "../utils";
 
 export default function handleRoomCommand({
   subCommand,
   commandParam,
-}: SubCommandParams) {}
+}: SubCommandParams) {
+  switch (subCommand) {
+    case "status":
+      if (!checkLocation([SokcetLocation.ROOM])) return;
+
+    case "start":
+      // 방장인지도 확인해야함
+      if (!checkLocation([SokcetLocation.ROOM])) return;
+      
+    case "list":
+      if (!checkLocation([SokcetLocation.LOBBY])) return;
+      
+    case "join":
+      if (!checkLocation([SokcetLocation.LOBBY])) return;
+
+    case "create":
+      if (!checkLocation([SokcetLocation.LOBBY])) return;
+  }
+}
